6236dccb9eb5ee27497e5bd7b823872308fd2751
Haim <hgq_2000@hotmail.com>
2022-11-16 08:38:39 -0600

    In process to create whatGPT3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './whatGPT3.css'
+
+import Feature from './../../Components/feature/Feature'
+
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id="wgpt3">
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
