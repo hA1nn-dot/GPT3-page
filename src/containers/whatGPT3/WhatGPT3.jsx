f7a68aae46ce880cfd2d750afb3a4c9452543fa6
Haim <hgq_2000@hotmail.com>
2022-11-16 12:19:39 -0600

    Fix gradient color

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -10,7 +10,7 @@ function WhatGPT3() {
         <Feature />
       </div>
       <div className='gpt3__whatgpt3-heading'>
-        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <p>Explore Library</p>
       </div>
       <div className='gpt3__whatgpt3-container'>
